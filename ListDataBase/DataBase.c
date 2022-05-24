9e18516f04681ec9f0572f06a33b66af918d56e7
Anton Borisov <borisov-anton-00@mail.ru>
2022-05-24 12:33:55 +0300

    Added backwards and forwards push

--- ListDataBase/DataBase.c
+++ ListDataBase/DataBase.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//Declaration of the LinkedList structure
 typedef struct DataBaseList{
 
     int index; 
@@ -9,6 +10,7 @@ typedef struct DataBaseList{
     struct DataBaseList *next;
 } list;
 
+//Creates an element (node) of LinkedList
 list *create_node(int set_index, char *set_value){
 
     list *node = (list *)malloc(sizeof(list));
@@ -20,9 +22,8 @@ list *create_node(int set_index, char *set_value){
     return node;
 }
 
-void database_insert(list **db, int set_index, char *set_value){
-
-//Insert at the end
+//Inserts forward
+void insert_fwd(list **db, int set_index, char *set_value){
 
     list *new_element = create_node(set_index, set_value);
 
@@ -30,23 +31,46 @@ void database_insert(list **db, int set_index, char *set_value){
     *db = new_element;
 }
 
+//Inserts backwards
+void insert_bkwd(list **db, int set_index, char *set_value){
+
+    list *new_element = create_node(set_index, set_value);
+
+    list *temp = *db;
+    while(temp -> next != NULL){
+        temp = temp -> next;
+    }
+
+    temp -> next = new_element;
+}
+
+//Finds and prints Values of LinkedList elements by their index
 void database_read(list *db, int id){
 
     int counter = 0; //This counter uses for printing Error in case there was no matching index
-    while(db != NULL){
+
+    while((db != NULL) && (db -> index <= id)){
         if((db -> index) == id){
             printf("%s\n", db -> Value);
         }
     db = db -> next;
     }
+
     if(counter == 0) puts ("NoSuchIndexException");
 }
+
 int main(){
 
-    list *db = create_node(0, "Bob");
-    database_insert(&db, -1, "Anton");
-    database_insert(&db, 1, "Aaaaa");
-    database_insert(&db, -1, "Atttt");
+    list *db = create_node(0, "Name");
+
+    insert_fwd(&db, -1, "Anton");
+    insert_fwd(&db, 1, "Aaaaa");
+    insert_fwd(&db, -1, "Atttt");
+
+    insert_bkwd(&db, 2, "Fjjjj");
+    insert_bkwd(&db, 2, "Foooo");
+    insert_bkwd(&db, 0, "Frrrr");
+
     database_read(db, 2);
 
     while (db != NULL){
